eda895867a0ae439c7404b471496009701c6f685
yesenia <aryesigon@gmail.com>
2024-07-23 02:00:39 -0300

    practica de gestion de informacion

--- src/repositories/UserRepository.ts
+++ src/repositories/UserRepository.ts
@@ -2,10 +2,13 @@ import { AppDataSource } from "../config/data-source";
 import { User } from "../entities/User";
 
 const UserRepository = AppDataSource.getRepository(User).extend({
-  async findById(id: number) {
+  findById: async function (id: number): Promise<User | null> {
     const user = this.findOneBy({ id });
-
     return user;
+  },
+  checkById: async function (id: number): Promise<boolean> {
+    const user = await this.findById(id);
+    return user !== null;
   },
 });
 
